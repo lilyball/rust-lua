cd4828a0922b3e81544d3a65365fddd3a6e38ee1
Kevin Ballard <kevin@sb.org>
2013-12-08 20:37:09 -0800

    Add example program showing table iteration

---
+++ examples/table.rs
@@ -0,0 +1,14 @@
+// Sample code that shows how to traverse a table.
+// We'll use it to traverse the global table
+
+extern mod lua;
+
+fn main() {
+    let mut L = lua::State::new();
+    L.open_base();
+    L.pushnil(); // push the first key
+    while L.next(lua::GLOBALSINDEX) {
+        println!("{} - {}", L.describe(-2).unwrap_or_default(), L.typename(-1));
+        L.pop(1); // remove the value, keep the key
+    }
+}
