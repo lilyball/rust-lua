78b4a60d1b81c5ae0a3a0ddfa79c664dfe3352fc
Kevin Ballard <kevin@sb.org>
2014-02-03 20:04:14 -0800

    Update for removal of io::io_error
    All I/O operations now return a Result.

--- examples/common.rs
+++ examples/common.rs
@@ -10,23 +10,25 @@ pub fn repl(L: &mut lua::State) {
     let stderr = &mut io::stderr() as &mut io::Writer;
     loop {
         L.settop(0); // clear the stack
-        write!(stdout, "> ");
-        stdout.flush();
-        let line = stdin.read_line();
-        let mut line = if line.is_some() { line.unwrap() } else { break };
+        let _ = write!(stdout, "> ");
+        let _ = stdout.flush();
+        let mut line = match stdin.read_line() {
+            Ok(line) => line,
+            Err(_) => break
+        };
         if line.starts_with("=") {
             line = format!("return {}", line.slice_from(1));
         }
         match L.loadbuffer(line, "=stdin") {
             Ok(_) => (),
-            Err(err) => { writeln!(stderr, "{}", err.to_str()); continue; }
+            Err(err) => { let _ = writeln!(stderr, "{}", err.to_str()); continue; }
         }
         match L.pcall(0, lua::MULTRET, 0) {
             Ok(_) => (),
             Err(_) => {
                 match L.tostring(-1) {
-                    Some(msg) => writeln!(stderr, "{}", msg),
-                    None => writeln!(stderr, "(error object is not a string)")
+                    Some(msg) => { let _ = writeln!(stderr, "{}", msg); }
+                    None => { let _ = writeln!(stderr, "(error object is not a string)"); }
                 }
             }
         }
@@ -37,8 +39,8 @@ pub fn repl(L: &mut lua::State) {
             match L.pcall(nargs, 0, 0) {
                 Ok(_) => (),
                 Err(_) => {
-                    writeln!(stderr, "error calling 'print' ({})",
-                             L.tostring(-1).unwrap_or_default());
+                    let _ = writeln!(stderr, "error calling 'print' ({})",
+                                     L.tostring(-1).unwrap_or_default());
                     continue;
                 }
             }
