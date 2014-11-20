5eaa5e58ce9b554b9b696151eec7fffd131bbe7f
Kevin Ballard <kevin@sb.org>
2014-11-19 17:50:14 -0800

    Switch to a Cargo build script

---
+++ build.rs
@@ -0,0 +1,14 @@
+use std::io::Command;
+use std::io::process::{InheritFd, Ignored};
+
+fn main() {
+    let mut cmd = Command::new("make");
+    cmd.arg("cargo-prep");
+    println!("running: {}", cmd);
+    assert!(cmd.stdin(Ignored)
+               .stdout(InheritFd(1))
+               .stderr(InheritFd(2))
+               .status()
+               .unwrap()
+               .success());
+}
