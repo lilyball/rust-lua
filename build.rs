1381fdeba2c947ff87ef4b4cd18374499d453476
Kevin Ballard <kevin@sb.org>
2015-01-24 19:24:08 -0800

    Fix build script for latest rustc
    Command needs to be printed with `{:?}` now instead of `{}`.

--- build.rs
+++ build.rs
@@ -6,7 +6,7 @@ use std::io::process::{InheritFd, Ignored};
 fn main() {
     let mut cmd = Command::new("make");
     cmd.arg("cargo-prep");
-    println!("running: {}", cmd);
+    println!("running: {:?}", cmd);
     assert!(cmd.stdin(Ignored)
                .stdout(InheritFd(1))
                .stderr(InheritFd(2))
